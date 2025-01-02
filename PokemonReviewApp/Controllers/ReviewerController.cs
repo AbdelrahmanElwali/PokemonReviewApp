48d324adc73c038993fa04e37084174c5c3101eb
AbdelrahmanElwali <boodiwali5@gmail.com>
2025-01-02 02:54:48 +0200

    done

--- PokemonReviewApp/Controllers/ReviewerController.cs
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -92,7 +92,7 @@ namespace PokemonReviewApp.Controllers
                 return StatusCode(500, ModelState);
             }
 
-            return Ok("Successfully created");
+            return Ok("Successfully created"); //done!!
         }
     }
 }
